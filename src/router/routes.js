607f5f5ece95e928eef23ac2d0511d7f79fec1fb
gabriellkerber <kerbergabriel@gmail.com>
2022-07-30 18:55:51 -0300

    Rotas e Criado Tela Login

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,17 @@
 
 const routes = [
   {
-    path: '/',
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }
     ]
   },
 
